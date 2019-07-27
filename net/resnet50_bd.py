d7666ab20bcecd2d2222a6ff86b616fd495abe24
Chuan Cen <chuancen@deeplearn9.eecs.umich.edu>
2019-07-26 20:07:23 -0400

    todo: new version.

--- net/resnet50_bd.py
+++ net/resnet50_bd.py
@@ -21,37 +21,39 @@ class Net(nn.Module):
         # branch: class boundary detection
         self.fc_edge1 = nn.Sequential(
             nn.Conv2d(64, 32, 1, bias=False),
+            nn.MaxPool2d(2),
             nn.GroupNorm(4, 32),
             nn.ReLU(inplace=True),
         )
         self.fc_edge2 = nn.Sequential(
             nn.Conv2d(256, 32, 1, bias=False),
+            nn.MaxPool2d(2),
             nn.GroupNorm(4, 32),
             nn.ReLU(inplace=True),
         )
         self.fc_edge3 = nn.Sequential(
             nn.Conv2d(512, 32, 1, bias=False),
             nn.GroupNorm(4, 32),
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
+            # nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
             nn.ReLU(inplace=True),
         )
         self.fc_edge4 = nn.Sequential(
             nn.Conv2d(1024, 32, 1, bias=False),
             nn.GroupNorm(4, 32),
-            nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
             nn.ReLU(inplace=True),
         )
         self.fc_edge5 = nn.Sequential(
             nn.Conv2d(2048, 32, 1, bias=False),
             nn.GroupNorm(4, 32),
-            nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
             nn.ReLU(inplace=True),
         )
         self.fc_edge6 = nn.Conv2d(160, 1, 1, bias=True)
         
         self.gap0 = Gap(2048, 20)
-        self.upscale_cam = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False)
-        self.infuse = BDInfusion(20, 20, rel_pattern=[(3,3),(3,1),(3,5),(5,1)])
+        self.upscale_cam = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.infuse = BDInfusion(20, 20, rel_pattern=[(3,3),(3,6),(5,1),(5,5)])
         self.nclass = 20
 
         self.backbone = nn.ModuleList([self.stage4, self.stage5])
