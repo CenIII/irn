bc50ab52a5919d9947b864d2bedade23d2bc51bb
Chuan Cen <chuancen@deeplearn9.eecs.umich.edu>
2019-10-19 23:42:55 -0400

    classifier changed to ASPP.

--- net/resnet50_cam.py
+++ net/resnet50_cam.py
@@ -38,7 +38,7 @@ class Net(nn.Module):
         self.stage3 = nn.Sequential(self.resnet50.layer3)
         self.stage4 = nn.Sequential(self.resnet50.layer4)
 
-        self.classifier = _ASPP(2048, 21, [6, 12, 18, 24]) #nn.Conv2d(2048, 20, 1, bias=False)
+        self.classifier = _ASPP(2048, 20, [6, 12, 18, 24]) #nn.Conv2d(2048, 20, 1, bias=False)
 
         self.backbone = nn.ModuleList([self.stage1, self.stage2, self.stage3, self.stage4])
         self.newly_added = nn.ModuleList([self.classifier])
@@ -52,10 +52,10 @@ class Net(nn.Module):
         x = self.stage4(x)  # N, 2048, 32, 32
         
 
-        # x = torchutils.gap2d(x, keepdims=True) # N, 2048, 1, 1
+        x = torchutils.gap2d(x, keepdims=True) # N, 2048, 1, 1
         x = self.classifier(x) # N, 20, 32, 32
         
-        # x = x.view(-1, 20) # N, 20
+        x = x.view(-1, 20) # N, 20
 
         return x
 
@@ -85,7 +85,8 @@ class CAM(Net):
 
         x = self.stage4(x)
 
-        x = F.conv2d(x, self.classifier.weight)
+        # x = F.conv2d(x, self.classifier.weight)
+        x = self.classifier(x)
         x = F.relu(x)
         # x = torchutils.leaky_log(x,leaky_rate=0.) #torch.log(1+F.relu(x))
         
